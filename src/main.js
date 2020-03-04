98eb594155178593e710c27f93ec71963fa2e693
yuxinn <yuxin.goh.2017@smu.edu.sg>
2020-03-04 21:33:03 +0800

    fetch table every 7 seconds and book table

--- src/main.js
+++ src/main.js
@@ -1,12 +1,15 @@
 import Vue from 'vue'
-import { router } from './router/router.js'
 import App from './App.vue'
-import Antd from 'ant-design-vue'
+import moment from 'moment'
 import axios from 'axios'
+import { router } from './router/router.js'
+
+import Antd from 'ant-design-vue'
 import 'ant-design-vue/dist/antd.css'
 
 Vue.use(Antd)
 Vue.config.productionTip = false
+Vue.prototype.moment = moment
 Vue.prototype.$http = axios
 
 new Vue({
--- src/pages/Dashboard/Dashboard.vue
+++ src/pages/Dashboard/Dashboard.vue
@@ -1,26 +1,33 @@
 <template>
   <div class="Dashboard mb-4">
     <p class="h4 mb-3">Hot Desk Summary</p>
+    <div class="d-flex justify-content-end mb-3">
+      <a-spin v-if="fetching" size="small" />
+      <a-tag v-else class="mr-0 pl-0" style="cursor: default;">
+        <a-icon type="check-circle" style="vertical-align: 1px;" class="mx-2"/>
+        Last Updated: {{time.format("MMMM Do YYYY, HH:mm:ss")}}
+      </a-tag>
+    </div>
     <div style="background-color: #ececec; padding: 20px;">
-      <a-list v-for="cluster in clusters" :key="cluster" itemLayout="horizontal" :dataSource="cluster">
-        <a-list-item slot="renderItem" slot-scope="item">
+      <a-list itemLayout="horizontal" :dataSource="clusters" :loading="firstload">
+        <a-list-item slot="renderItem" slot-scope="cluster">
           <a-list-item-meta>
-            <div slot="title">Cluster {{item}}</div>
+            <div slot="title">{{cluster.toUpperCase()}}</div>
             <div slot="description">
               {{clusterStatus(tables[cluster])}}
             </div>
           </a-list-item-meta>
           <div class="content">
             <div class="tables row">
-              <div class="col-auto mr-3" v-for="{tableid, status} in tables[cluster]" :key="tableid">
+              <div class="col-auto mr-3" v-for="{tableId, status} in tables[cluster]" :key="tableId">
                 <div class="row d-flex justify-content-center">
                   <div :class="'circle ' + status">
                     <a-popconfirm
                       v-if="status=='available'"
                       placement="topRight"
-                      :title="`Book ${tableid}?`"
+                      :title="`Book ${tableId.toUpperCase()}?`"
                       okText="Book"
-                      @confirm="() => bookTable(cluster, tableid)"
+                      @confirm="() => bookTable(cluster, tableId)"
                     >
                       <a-icon slot="icon" type="tag" style="color: dimgray" />
                       <button class="book-btn" href="javascript:;" ></button>
@@ -28,7 +35,7 @@
                   </div>
                 </div>
                 <div class="row">
-                  {{tableid}}
+                  {{tableId.toUpperCase()}}
                 </div>
               </div>
             </div>
@@ -41,85 +48,52 @@
 </template>
 
 <script>
-import { getTables, createTable,  } from "../../api"
+import { getTables, createTable, bookTable } from "../../api"
+import moment from 'moment'
 
 export default {
-  components: {
-
-  },
   data() {
     return {
-        tables: {},
-        tableid: "",
-        status: "available"
+      tables: {},
+      firstload: true,
+      fetching: true,
+      time: moment()
     }
   },
   mounted(){
-    // this.getTables()
-    this.tables = {
-        A: [
-          {
-          tableid: "a_01",
-          status: "booked"
-          },
-          {
-          tableid: "a_02",
-          status: "taken"
-          },
-          {
-          tableid: "a_03",
-          status: "available"
-          },
-          {
-          tableid: "a_04",
-          status: "hogging"
-          },
-          {
-          tableid: "a_05",
-          status: "available"
-          }
-        ],
-        B: [
-          {
-          tableid: "b_01",
-          status: "taken"
-          },
-          {
-          tableid: "b_02",
-          status: "taken"
-          }
-        ],
-        C: [
-          {
-          tableid: "c_01",
-          status: "taken"
-          },
-          {
-          tableid: "c_02",
-          status: "hogging"
-          }
-        ]
-      }
+    this.getTables()
+  },
+  created() {
+    this.interval = setInterval(() => {
+      this.getTables()
+    }, 7000);
   },
   methods: {
     async getTables() {
-      var resp = await getTables()
-      this.tables = resp
-      console.log(resp)
+      try {
+        this.fetching = true
+        var resp = await getTables()
+        this.tables = resp
+        this.time = moment()
+      } catch {
+        this.$message.error(`Error retrieving table details.`);
+      } finally {
+        this.fetching = false
+        this.firstload = false
+      }
     },
     async createTable() {
-      var data = { tableid: this.tableid, status: this.status }
+      var data = { tableId: this.tableId, status: this.status }
       var resp = await createTable(data)
       console.log(resp)
     },
     async bookTable(cluster, id) {
       try {
-        // await bookTable(tableid)
+        await bookTable(id)
         const tables = this.tables
-        var table = tables[cluster].find( ({ tableid }) => tableid === id );
+        var table = tables[cluster].find( ({ tableId }) => tableId === id );
         table.status = "booked"
-
-        this.$message.success(`${id} will be reserved for an hour.`);
+        this.$message.success(`${id.toUpperCase()} booked successfully will be reserved for an hour.`);
       } catch(err) {
         console.error(err)
       }
@@ -169,8 +143,11 @@ export default {
   .taken {
     background: rgb(211, 8, 75);
   }
+  .away {
+    background: rgb(255, 209, 82);
+  }
   .hogging {
-    background: rgb(251, 155, 29);
+    background: rgb(251, 129, 29);
   }
   .availabletext {
     color: #63C3A7;
